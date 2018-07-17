356ae5a227f8aca32c0a09aa14297fce029beaf0
Alex <humsterman86@outlook.com>
2018-07-17 08:47:44 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import argparse
+import pika
+import sys
+import logging
+
+def print_data(channel, method, properties, body):
+    print 'body:', body
+
+__author__ = 'humsterman86'
+
+parser = argparse.ArgumentParser(description='Simple utility for RabbitMQ management')
+parser.add_argument('-q','--queue', help='Get queue messages by name', required=True)
+
+args = parser.parse_args()
+
+if args.queue:
+    parameters = pika.ConnectionParameters('localhost')
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+    channel.queue_declare(queue=args.queue, exclusive=False,auto_delete=False)
+    channel.basic_consume(print_data, queue=args.queue)
+    channel.start_consuming()
+    connection.close()
